16dccaec2f2c6f2642c857c3321be02cf2c9e13f
Ebuka Ejiofor <ebuskelele@gmail.com>
2023-03-29 14:20:51 +0100

    optimize code for vercel

--- api/index.js
+++ api/index.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { generateBadgeUrl } = require("./shields");
-const quips = require("./quips.json");
-const axios = require("axios");
+const quips = require("../quips.json");
+
 app.use(cors());
 
+const { v4 } = require("uuid");
+
+app.get("/api", (req, res) => {
+	const path = `/api/item/${v4()}`;
+	res.setHeader("Content-Type", "text/html");
+	res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+	res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
 // Get all the quips
-app.get("/quips", (request, response) => {
+app.get("/package-lock.jsonapi/quips", (request, response) => {
 	const data = { quips };
 	response.json(data);
 });
 
 // Get only one random quip
-app.get("/quips/random", (request, response) => {
+app.get("/api/quips/random", (request, response) => {
 	const randomQuip = quips[Math.floor(Math.random() * quips.length)];
 	response.json(randomQuip);
 });
